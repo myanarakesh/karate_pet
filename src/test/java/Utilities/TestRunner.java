502a471ecc0a8862fdafe2e34aabdcbb5f7d29f0
unknown <myanarakesh@outlook.com>
2020-10-27 11:52:15 +0530

    updated for cucumber report

--- src/test/java/Utilities/TestRunner.java
+++ src/test/java/Utilities/TestRunner.java
@@ -1,20 +1,15 @@
 package Utilities;
 
 import static org.junit.Assert.assertTrue;
-
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-
 import org.apache.commons.io.FileUtils;
 import org.junit.Test;
-
 import com.intuit.karate.KarateOptions;
 import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
-
-import cucumber.api.CucumberOptions;
 import net.masterthought.cucumber.Configuration;
 import net.masterthought.cucumber.ReportBuilder;
 
@@ -23,6 +18,17 @@ import net.masterthought.cucumber.ReportBuilder;
 public class TestRunner {
 	@Test
 	public void testParallel() {
-		Results results = Runner.parallel(getClass(), 5, "E:\\java neon workspace\\petShop\\target");
+		Results results = Runner.parallel(getClass(), 5, "./Reports/target");
+		generateReport(results.getReportDir());
+		assertTrue(results.getErrorMessages(), results.getFailCount() == 0);
+	}
+
+	public static void generateReport(String karateOutputPath) {
+		Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[] { "json" }, true);
+		List<String> jsonPaths = new ArrayList<String>(jsonFiles.size());
+		jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
+		Configuration config = new Configuration(new File("target"), "Pet Store Test cases");
+		ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
+		reportBuilder.generateReports();
 	}
 }
