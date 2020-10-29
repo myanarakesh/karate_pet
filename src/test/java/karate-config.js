2ff62355faac5c114a2a1cb83f5a551298e689d8
Admin <myanarakesh@outlook.com>
2020-10-29 09:18:50 +0530

    updated karate config file to fetch url from diff env

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -1,8 +1,18 @@
-function() {
-	return{
-		baseUrl: 'https://petstore.swagger.io/'
+function fn() {
+	var env = karate.env;
+	karate.log('karate.env system property was:', env);
+	if (!env) {
+		env = 'dev';
+	}
+	var config = {
+		baseUrl : 'https://petstore.swagger.io/'
 	};
-	
-	  karate.log('karate.env =', karate.env);
-	  karate.log('config.baseUrl =', config.baseUrl);
+	if (env == 'stage') {
+		config.baseUrl = 'https://stage.petstore.swagger.io/';
+	} else if (env == 'qa') {
+		config.baseUrl = 'https://petstore.swagger.io/';
+	}
+	karate.configure('connectTimeout', 5000);
+	karate.configure('readTimeout', 5000);
+	return config;
 }
